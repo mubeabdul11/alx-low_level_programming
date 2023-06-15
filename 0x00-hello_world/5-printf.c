3941d3c18090dd885cfd210c3e8f491518eed81e
mubarek <mubeabdul11@gmail.com>
2023-06-15 15:09:00 -0700

    Task6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Retun Always 0 (Success)
+*/
+int main(void)
+{
+      print("with proper grammar but the outcome is a piece of art,\n");
+      retun(0);
+}
