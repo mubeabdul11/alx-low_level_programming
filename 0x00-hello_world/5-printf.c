3acdc2c188a2f44ce7f521bc34feee181ab51716
mubarek <mubeabdul11@gmail.com>
2023-06-15 15:19:19 -0700

    Task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
 *
 *Retun Always 0 (Success)
 */
+
 int main(void)
 {
-      print("with proper grammar but the outcome is a piece of art,\n");
+      printf("with proper grammar but the outcome is a piece of art,\n");
       return(0);
 }
