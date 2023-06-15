d96bc911e62861340609a7cfdaa6f9c426fd0a4f
mubarek <mubeabdul11@gmail.com>
2023-06-15 14:06:14 -0700

    Task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
- * main - A program that prints with a new line with pust function
+ * main - A program that prints a line with pusts function
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+puts("Programming is like building a multilingual puzzle")
 return (0);
 }
