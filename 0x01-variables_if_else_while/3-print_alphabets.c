28a4a50f38a762e85c63a45345ceb01630ea400a
mubarek <mubeabdul11@gmail.com>
2023-06-16 19:35:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar (c);
+			c++;
+		}
+	while 
+		(d <= 'z')
+		{
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
